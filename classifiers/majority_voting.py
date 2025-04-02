3b57ea7d5929fd5dc97c76762eea0e18463ab25c
Arjun Sudheer <arjunsudheer05@outlook.com>
2025-04-02 01:04:08 -0700

    Add ids-agent implementation
    Remove ids-agent call from majority voting implementation

--- classifiers/majority_voting.py
+++ classifiers/majority_voting.py
@@ -12,6 +12,7 @@ from sklearn.metrics import (
     confusion_matrix,
 )
 import os
+import joblib
 
 from rfc import RFCNetworkAttackClassifier
 from svm import SVMNetworkAttackClassifier
@@ -27,11 +28,8 @@ os.chdir(project_root)
 
 
 class MajorityVoting:
-    def __init__(self, dataset_directory, label_column):
+    def __init__(self, dataset_directory):
         self.dataset_directory = dataset_directory
-        self.label_column = label_column
-
-        self.majority_prediction_key = "majority_predictions"
 
         self.__load_data()
 
@@ -62,19 +60,27 @@ class MajorityVoting:
         train_df = pd.read_csv(f"{self.dataset_directory}/train.csv")
         test_df = pd.read_csv(f"{self.dataset_directory}/test.csv")
 
-        # Convert labels to int64 (ensure proper encoding if categorical)
-        self.y_train = train_df[self.label_column].astype(np.int64).values
-        self.y_test = test_df[self.label_column].astype(np.int64).values
+        with open(f"{self.dataset_directory}/num_y_columns.txt", "r") as f:
+            num_y_train_cols = int(f.readline())
+            num_y_test_cols = int(f.readline())
 
-        # Convert features to float32 (compatible with both scikit-learn and PyTorch)
-        self.X_train = (
-            train_df.drop(columns=[self.label_column]).astype(np.float32).values
-        )
-        self.X_test = (
-            test_df.drop(columns=[self.label_column]).astype(np.float32).values
-        )
+        # Separate features and labels
+        y_train = train_df.iloc[:, -num_y_train_cols:]
+        self.X_train = train_df.iloc[:, :-num_y_train_cols]
 
-    def __get_classifier_predictions(self, samples):
+        y_test = test_df.iloc[:, -num_y_test_cols:]
+        self.X_test = test_df.iloc[:, :-num_y_test_cols]
+
+        # Convert one-hot to single-label
+        self.y_train = np.argmax(y_train.values, axis=1)
+        self.y_test = np.argmax(y_test.values, axis=1)
+
+        # Load the LabelEncoder from the pickle file
+        label_binarizer_path = f"{self.dataset_directory}/label_binarizer.pkl"
+        with open(label_binarizer_path, "rb") as file:
+            self.label_binarizer = joblib.load(file)
+
+    def get_classifier_predictions(self, samples):
         predictions = {}
 
         predictions["rfc"] = self.rfc.predict_network_attack_class(samples)
@@ -87,43 +93,35 @@ class MajorityVoting:
 
         return predictions
 
-    def calculate_majority_classification(self, samples):
-        predictions = self.__get_classifier_predictions(samples)
-        # Keep track of the majority predictions
-        majority_predictions = []
-
-        # Majority voting
-        for i in range(len(samples)):
-            current_predictions = []
-            for key in predictions:
-                current_predictions.append(predictions[key][i])
-
-            occurrences = Counter(current_predictions)
-            majority_prediction = occurrences.most_common(1)[0][0]
-            # Calculates how many classifiers agreed on the majority prediction
-            agreement_ratio = occurrences[majority_prediction] / len(
-                current_predictions
-            )
-
-            # Prompt IDS-Agent if the agreement ratio is below a predefined threshold
-            if agreement_ratio < (5 / 7):
-                # Update the majority prediction with the prediction from IDS-Agent
-                pass
-
-            majority_predictions.append(majority_prediction)
-
-        # Update predictions dictionary with majority prediction
-        predictions[self.majority_prediction_key] = majority_predictions
-
-        return predictions
-
     def calculate_classification_metrics(self):
+        def calculate_majority_classification(majority_prediction_key):
+            predictions = self.get_classifier_predictions(self.X_test)
+            # Keep track of the majority predictions
+            majority_predictions = []
+
+            # Majority voting
+            for i in range(len(self.X_test)):
+                current_predictions = []
+                for key in predictions:
+                    current_predictions.append(predictions[key][i])
+
+                occurrences = Counter(current_predictions)
+                majority_prediction = occurrences.most_common(1)[0][0]
+                majority_predictions.append(majority_prediction)
+
+            # Update predictions dictionary with majority prediction
+            predictions[majority_prediction_key] = majority_predictions
+
+            return predictions
+
+        # Get classifier predictions
+        predictions = calculate_majority_classification(
+            majority_prediction_key="majority_predictions"
+        )
+
         # Create the directory for saving metrics, if it doesn't exist
         metrics_save_directory = Path(f"{self.dataset_directory}/classifier_metrics")
         metrics_save_directory.mkdir(parents=True, exist_ok=True)
-
-        # Get classifier predictions
-        predictions = self.calculate_majority_classification(self.X_test)
 
         # Save classification metrics to a file
         for key in predictions:
@@ -152,12 +150,12 @@ class MajorityVoting:
 
 
 if __name__ == "__main__":
-    datasets = {
-        "datasets/nsl_kdd": "labels",
-        "datasets/aci_iot_network_dataset_2023": "Label",
-        "datasets/cic_iot_dataset_2023": "label",
-    }
+    datasets = [
+        "datasets/nsl_kdd",
+        "datasets/aci_iot_network_dataset_2023",
+        "datasets/cic_iot_dataset_2023",
+    ]
 
-    for dataset_directory, label_column in datasets.items():
-        majority_vote_classifier = MajorityVoting(dataset_directory, label_column)
+    for dataset_directory in datasets:
+        majority_vote_classifier = MajorityVoting(dataset_directory)
         majority_vote_classifier.calculate_classification_metrics()
